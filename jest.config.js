907c880ccb6fde1e65aa6af4856d82ca50c5c1ab
Joao Paulo Monteiro <jcavalcante.bairesdev@gasinc.us>
2023-09-21 15:23:39 -0300

    remove stories and index files from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**',
+    '!src/**/*.stories.tsx',
+    '!src/components/**/index.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
